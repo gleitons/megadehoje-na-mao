c8f1d786ddad5aaef856335d3740946265dd286b
gleitons <31367377+gleitons@users.noreply.github.com>
2021-11-07 05:58:20 -0300

    atua

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,31 @@
+var numerosMegadaTabela = document.getElementById('div#numerosMegaSTabela');
+var numerosEscolhidos = [];
+var botaojogar = document.getElementById('gerarnumerosT')
+numerosMegadaTabela.addEventListener('click', function(cordefundo){
+        cordefundo.target.style.background = "#4af050";
+        numerosEscolhidos.push(cordefundo.target.innerHTML);
+        if (numerosEscolhidos.length > 6){
+            alert('Escolha somente 06 numeros');
+            cordefundo.target.style.background = "#6f6f6f";
+            botaojogar.disabled = false;
+            return;
+        }
+        validanumerosescolhidos();
+})
+function validanumerosescolhidos() {
+    if(numerosEscolhidos.length == 6){
+        botaojogar.disabled = false;
+    }
+}
+
+
+
+
+
+
+
+
+
 const btnMobileM = document.querySelector('button#botaoMobileP')
     
 
