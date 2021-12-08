9558a31db2b2a9ceab7af0fd7cd9de17c3b69cb9
gleitons <31367377+gleitons@users.noreply.github.com>
2021-12-08 06:40:41 -0300

    2435

--- scripts/resultados.js
+++ scripts/resultados.js
@@ -1,6 +1,37 @@
-function f(){
-    let num = 8
-    num** = 2
+function mostrarMcais(){
+    var verMais = document.getElementById('verMais');
+
     
-    mostrar.innerHTML = `${num}`
+    if(verMais == false){
+        document.getElementById("verMais").style.display = "block";
+        var agoraFecha = false;
+    }else {
+        document.getElementById("verMais").style.display = "none";
+        var agoraFecha = true;
+    }
+    
+
+
+}
+
+
+var visibilidade = true; //Variável que vai manipular o botão Exibir/ocultar
+
+function exibir() {
+    document.getElementById("verMais").style.display = "block";
+}
+
+function ocultar() {
+    document.getElementById("verMais").style.display = "none";
+}
+
+function mostrarMais() { // Quando clicar no botão.
+
+    if (visibilidade) {//Se a variável visibilidade for igual a true, então...
+        document.getElementById("verMais").style.display = "none";//Ocultamos a div
+        visibilidade = false;//alteramos o valor da variável para falso.
+    } else {//ou se a variável estiver com o valor false..
+        document.getElementById("verMais").style.display = "block";//Exibimos a div..
+        visibilidade = true;//Alteramos o valor da variável para true.
+    }
 }
