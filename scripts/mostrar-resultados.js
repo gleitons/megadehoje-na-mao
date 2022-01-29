bfd33a7c742399d36c570fbc0ca4be3819d39b91
gleitons <31367377+gleitons@users.noreply.github.com>
2022-01-28 21:04:55 -0300

    24472

---
+++ scripts/mostrar-resultados.js
@@ -0,0 +1,12 @@
+function ocultarF() {
+    var styles = window.getComputedStyle(
+        document.getElementsByClassName('bc1'),
+        ':before'
+      )
+      console.log(styles);
+    // const verm = document.getElementById('bConferir').getElementsByClassName('bc1');
+
+    // verm.dataset.content = 'Muda valor da propriedade, e isso reflete no pseudo-elemento';
+    
+    
+}
